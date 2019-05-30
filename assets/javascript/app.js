28dc7279ad2a5e8084b10cf6fd7d8a68d4dc340c
Juwami <sean.nguyenf@yahoo.com>
2019-05-29 22:01:04 -0400

    add firebase info from google

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDFZpUTsrZDFLc4fzswF7_2zilHSiqObwU",
+    authDomain: "train-scheduler-a30e5.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a30e5.firebaseio.com",
+    projectId: "train-scheduler-a30e5",
+    storageBucket: "train-scheduler-a30e5.appspot.com",
+    messagingSenderId: "457857000350",
+    appId: "1:457857000350:web:4649510b9b490eba"
+};
+
+firebase.initializeApp(firebaseConfig)
