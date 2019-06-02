7cf0b4fbf17199d7dd4d8d61e2af0f60cf8058d2
Juwami <sean.nguyenf@yahoo.com>
2019-06-01 21:50:06 -0400

    added placeholders fro html inputs and added current time/date for UI

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,3 @@
-let $trainNameInput = $('.trainNameInput').val().trim()
-let $destinationInput = $('.destinationInput').val().trim()
-let $firstTrainTimeInput = $('.firstTrainTimeInput').val().trim()
-let $frequencyInput = $('.frequencyInput').val().trim()
-
-let trainEntered = {
-    trainname: $trainNameInput,
-    destination: $destinationInput,
-    firstTrain: $firstTrainTimeInput,
-    frequency: $frequencyInput
-}
-
 const firebaseConfig = {
     apiKey: "AIzaSyDFZpUTsrZDFLc4fzswF7_2zilHSiqObwU",
     authDomain: "train-scheduler-a30e5.firebaseapp.com",
@@ -24,8 +12,27 @@ firebase.initializeApp(firebaseConfig)
 
 let database = firebase.database()
 
+$(document).ready(function () {
+    $('#currentTime').html(moment().format('D/MMM/YYYY HH:mm:ss'))
+    updateTime()
+    setInterval(updateTime, 1000)
+
+})
+
 $('#submitBtn').on('click', function () {
     event.preventDefault()
+
+    let $trainNameInput = $('.trainNameInput').val().trim()
+    let $destinationInput = $('.destinationInput').val().trim()
+    let $firstTrainTimeInput = $('.firstTrainTimeInput').val().trim()
+    let $frequencyInput = $('.frequencyInput').val().trim()
+
+    let trainEntered = {
+        trainName: $trainNameInput,
+        destination: $destinationInput,
+        firstTrain: $firstTrainTimeInput,
+        frequency: $frequencyInput
+    }
 
     console.log(trainEntered)
 
@@ -37,8 +44,29 @@ $('#submitBtn').on('click', function () {
     $('.frequencyInput').val('')
 })
 
-database.ref().on('child_added', function(snapshot) {
+database.ref().on('child_added', function (snapshot) {
     console.log(snapshot.val())
 
-    
+    let destination = snapshot.val().destination
+    let firstTrain = snapshot.val().firstTrain
+    let trainFrequency = snapshot.val().frequency
+    let trainName = snapshot.val().trainName
+
+    console.log(destination)
+    console.log(firstTrain)
+    console.log(trainFrequency)
+    console.log(trainName)
+
+    let newTrainRow = $('<tr>').append(
+        $('<td>').text(trainName).addClass('text-center'),
+        $('<td>').text(destination).addClass('text-center'),
+        $('<td>').text(trainFrequency).addClass('text-center'),
+        $('<td>').text('next arrival: current time + fre').addClass('text-center'),
+        $('<td>').text('minutes away').addClass('text-center')
+    )
+    $('#trainScheduleBody').append(newTrainRow)
 })
+
+let updateTime = function () {
+    $('#currentTime').html(moment().format('D/MMM/YYYY HH:mm:ss'))
+}
